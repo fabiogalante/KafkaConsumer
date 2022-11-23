4b83cdb39e5739cabc52464db3b432751c4fd98b
Fabio Galante Manse <fabiogalantemans@gmail.com>
2022-11-22 21:53:00 -0300

    Kafka

---
+++ KafkaConsumer/Controllers/KafkaProducerController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KafkaConsumer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class KafkaProducerController : ControllerBase
+    {
+        private readonly ProducerConfig config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+        private readonly string topic = "simpletalk_topic";
+        [HttpPost]
+        public IActionResult Post([FromQuery] string message)
+        {
+            return Created(string.Empty, SendToKafka(topic, message));
+        }
+
+        private Object? SendToKafka(string topic, string message)
+        {
+            using (var producer =
+                 new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    return producer.ProduceAsync(topic, new Message<Null, string> { Value = message })
+                        .GetAwaiter()
+                        .GetResult();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Oops, something went wrong: {e}");
+                }
+            }
+            return null;
+        }
+
+
+    }
+}
